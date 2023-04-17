bdaa98df5bd42eb90c522d5c85d50613fec988aa
Luigidefilippo <defilippoluigi65@gmail.com>
2023-04-17 22:16:39 +0200

    add layout loader and Header and main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script setup>
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppLoader from "./components/AppLoader.vue";
+import { store } from "./components/store";
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppLoader
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
 
 <template>
-<h1>ciao</h1>
+  <AppHeader />
+  <AppMain />
 </template>
 
-<style scoped>
-
-
+<style lang="scss">
+@use "./components/styles/general.scss";
 </style>
+
