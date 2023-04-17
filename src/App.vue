6ec8b402b4dcaa76387dff28b7dcfa6bd67d2038
Luigidefilippo <defilippoluigi65@gmail.com>
2023-04-17 22:22:34 +0200

    add axios

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppLoader from "./components/AppLoader.vue";
 import { store } from "./components/store";
+import axios from "axios";
 export default {
   components: {
     AppHeader,
@@ -13,6 +14,11 @@ export default {
     return {
       store,
     };
+  },
+  mounted() {
+    axios.get(store.apiURL).then((resp) => {
+      this.store.cards = resp.data.data;
+    });
   },
 };
 </script>
