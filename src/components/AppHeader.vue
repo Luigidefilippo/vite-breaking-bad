d8b3b77e10318446c75024cb0bd468e43b177976
Luigidefilippo <defilippoluigi65@gmail.com>
2023-04-17 22:46:18 +0200

    finish header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,10 +6,23 @@ export default{
 
 
 <template>
-<h1>Yu-Gi-Oh Api</h1>
+  <div class="container d-flex align-items-center">
+    <img src="../assets/ygoprodeck_header_logo.png" alt="">
+    <h1>Yu-Gi-Oh Api</h1>
+  </div>
 </template>
+
+
 <style scoped lang="scss">
-h1 {
-  font-weight: 900;
+.container{
+  height: 80px;
+  padding: 10px;
+  h1{
+    font-weight: 800;
+  }
+  img{
+    width: 100px;
+  }
+
 }
 </style>
